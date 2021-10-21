50180554ba7782f9c8a4c5fe1e1c99337f8ed5d0
dnp987 <dnp987@gmail.com>
2021-10-21 16:43:37 -0400

    updated index.js, added server.js, test3.html

---
+++ server.js
@@ -0,0 +1,22 @@
+// Initialize App
+const { resolveInclude } = require('ejs');
+const express = require('express');
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+// Assign route
+app.get('/', (req, res) => {
+    var name = 'hello';
+    res.render(__dirname + '/test3.html', {name:name});
+  });
+
+  app.get('/test_api', (req, res) => {
+    res.json({title:"api", message:"test"});
+  });
+
+// Start server
+app.listen(5000, () => {
+    console.log('App listening on port 5000');
+  });
