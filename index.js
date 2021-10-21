3334e2e0b11338b76a3c94b7de28d4f9b44025b2
dnp987 <dnp987@gmail.com>
2021-10-21 16:44:40 -0400

    Updated with proper pug view engine and views set

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 // Initialize App
 const express = require('express');
 const app = express();
-app.use(express.urlencoded({ extended: true })); 
+app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'pug');  // if you don't use this, then you use specify the .pug file extension.
+app.set('views', './views'); // views is the default if app.set('views', './views') is not used. Use this if you use a different directory.
 
 // Assign route
 app.get('/', (req, res) => {
   var name = "one";
   var number = 123;
-  res.render('index.pug', {name, number});
+  res.render('index', {name, number});
 });
 
 app.get('/test', (req, res) => {
-  var testname = req.query.testname;
-  var done = testname;
+  var done = req.query.testname;
   console.log ("GET:", done);
-  res.render('index2.pug', {done});
+  res.render('index2', {done});
 });
 
 app.post('/test2', (req, res) => {
-  var testname2 = req.body.testname2;
-  var done = testname2;
+  var done = req.body.testname2;
   console.log ("POST:", done);
-  res.render('index2.pug', {done});
+  res.render('index2', {done});
 });
   
 // Start server
