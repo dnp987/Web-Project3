d74ac7526f928d5bfb2414da4c92ed43c6442772
dnp987 <dnp987@gmail.com>
2021-10-24 23:51:03 -0400

    On going changes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 //const { resolveInclude } = require('ejs');
 const express = require('express');
 const app = express();
+var PORT = 5000;
 app.use(express.static('./'));
 //app.use(express.urlencoded({ extended: true })); you only need this if you're sending data from an html form
 app.use(express.json());
@@ -19,7 +20,10 @@ app.get('/test_get', (req, res) => {
   console.log(req.query);
   console.log(req.query.test);
   console.log(req.query.test2);
-  res.json({title:"get", message:"test"});
+  var test = req.query.test;
+  var test2 = req.query.test2;
+  res.json({title:"get " + test, message:"test " + test2});
+  res.end();
 });
 
 app.post('/test_post',  (req,res) => {
@@ -27,10 +31,13 @@ app.post('/test_post',  (req,res) => {
   console.log(req.body);
   console.log(req.body.test);
   console.log(req.body.test2);
-  res.json({title:"post", message:"test2"});
+  var test = req.body.test;
+  var test2 = req.body.test2;
+  res.json({title:"post "+ test, message:"test2 " + test2});
+  res.end();
 })
 
 // Start server
-app.listen(5000, () => {
+app.listen(PORT, () => {
     console.log('App listening on port 5000');
 });
